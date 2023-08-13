b1c5cffaac126aa5111420f41cc712d063e4ca58
Mahdi Valadan <94804009+MahdiValadan@users.noreply.github.com>
2023-08-13 22:31:37 +0200

    add alpine

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-$(document).ready(function(){
-    $('#nav-button').click(function (){
-        let navbar = $('#navbar')
-        $(this).addClass('animation-swing');
-        $(this).one('animationend', function(event) {
-            $(this).removeClass('animation-swing')
-        });
-        if(navbar.css("width") !== "0px"){
-            navbar.css("width","0%")
-            $("#nav-button-icon").text("menu");
-        }
-        else {
-            if (window.innerHeight > window.innerWidth){
-                navbar.css("width","100%")
-            }
-            else{
-                navbar.css("width","300px")
-            }
-            $("#nav-button-icon").text("close");
-        }
-    })
-    $('.nav-link').click(function (){
-        if(window.innerHeight > window.innerWidth){
-            $('#navbar').css("width","0%")
-            $("#nav-button-icon").text("menu");
-        }
-    })
+let programmingLangs = ['HTML5', 'CSS', 'JavaScript', 'Python', 'PHP', 'Java', 'C/C++', 'x86 Assembly', 'MATLAB']
+let frameworks = ['jQuery', 'React', 'Vue', 'Nuxt', 'Express', 'MongoDB', 'MySQL', 'Tailwind']
 
+let width = '350px'
+// Check whether user device is vertical or horizontal 
+if (window.innerHeight > window.innerWidth) {
+    width = '100%'
+}
+document.addEventListener('alpine:init', () => {
+    Alpine.data('menu', () => ({
+        open: false,
+        icon: 'menu',
+        menuWidth: '0%',
+        toggle() {
+            this.open = !this.open
+            if (this.open) {
+                this.icon = 'close'
+                this.menuWidth = width
+            } else {
+                this.icon = 'menu'
+                this.menuWidth = '0%'
+            }
+            const icon = document.getElementById("nav-button-icon");
+            icon.classList.add('animation-swing')
+            icon.addEventListener("animationend", () => {
+                icon.classList.remove("animation-swing")
+            })
+        },
+    }))
 })
